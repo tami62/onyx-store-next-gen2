af573ced1fcf50363dbeabd946693f8afd10e6fd
tamiz <tamilselvan28052001@gmail.com>
2024-10-02 22:22:50 +0530

    update storage resource

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -5,7 +5,7 @@ export const storage = defineStorage({
   access: (allow) => ({
     "product-images/{entity_id}/*": [
       allow.guest.to(["read"]),
-      allow.authenticated.to(["read"]),
+      //allow.authenticated.to(["read"]),
       allow.entity('identity').to(['read', 'write', 'delete'])
       //allow.groups(["Admins"]).to(["read", "write", "delete"]),
     ],
