13638dd646c711e86ac1958bd90ff5d846018ba7
tamiz <tamilselvan28052001@gmail.com>
2024-10-03 20:38:31 +0530

    update new

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,7 +4,7 @@ export const storage = defineStorage({
   name: "onyxStoreNextGen2Bucket",
   access: (allow) => ({
     "product-images/{entity_id}/*": [
-      allow.guest.to(["read"]),
+      allow.guest.to(['read', 'write', 'delete']),
       //allow.authenticated.to(["read"]),
       allow.entity('identity').to(['read', 'write', 'delete'])
       //allow.groups(["Admins"]).to(["read", "write", "delete"]),
